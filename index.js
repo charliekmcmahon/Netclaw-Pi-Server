afc9e9e58625a2f768a45cdca38c08550f781f7e
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2021-08-22 02:42:24 +0000

    Added API listner

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
 
 const Gpio = require('onoff').Gpio;
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+// Valid commands
+const validCommands = ["up", "down", "left", "right", "go", "start", "drop", "stop"];
+
+// Express consts
 const app = express();
 const port = 8000;
 
@@ -10,15 +18,39 @@ const port = 8000;
 var command;
 var auth;
 
+// Express options
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.get('/', (req, res) => {
   res.send('This is a NetClaw node. Please go to netclaw.com.au to play.')
 })
 
 app.post('/api', (req, res) => {
-	command = req;
-	console.log(command);
+	command = req.body.command;
+	console.log(`Command received: ${command}`);
+
+	try
+	{
+			if (validCommands.includes(command)) {
+				// Send GPIO based on command
+
+				res.sendStatus(200); // Send OK
+			}
+			else {
+				throw('Unrecognised command');
+			}
+			
+	}
+	catch(err)
+	{
+			// An error occured
+			res.sendStatus(400); // Send request error - 400
+	}
+
 	
-	res.send('Success');
 	
 })
 
